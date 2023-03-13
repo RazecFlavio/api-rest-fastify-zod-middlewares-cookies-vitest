f49e649ebc64d372b0f7fa0050c472510dcced8a
RazecFlavio <r4zecflavio@gmail.com>
2023-03-13 17:50:07 -0300

    iniciando api rest

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async (req, res) => {
+  const tables = await knex('sqLite_schema').select('*')
+  return tables
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running')
+  })
